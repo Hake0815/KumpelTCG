08be1058594b9b20492d5ac97de7f245d649f25f
Hake0815 <felix.reichenbach5@web.de>
2025-03-16 03:50:05 +0100

    \#1 Add Player 2 Hand and end turn button

--- Assets/Scripts/views/HandView.cs
+++ Assets/Scripts/views/HandView.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using DG.Tweening;
-using Mono.Cecil;
-using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
 
@@ -10,46 +8,52 @@ public class HandView : MonoBehaviour
 {
     private DeckView deck;
     private SplineContainer splineContainer;
-    private CardView cardPrefab;
+    private IPlayer player;
 
-    private List<CardView> handCards = new();
-
-    public void SetUp(DeckView deck, CardView cardPrefab)
+    public void SetUp(DeckView deck)
     {
         this.deck = deck;
-        this.cardPrefab = cardPrefab;
         splineContainer = GetComponent<SplineContainer>();
     }
 
     public void Register(IPlayer player)
     {
-        player.CardDrawn += HandleCardDrown;
+        player.CardDrawn += HandleCardDrawn;
+        this.player = player;
     }
 
-    public void HandleCardDrown(object player, List<ICard> drawnCards)
+    public void HandleCardDrawn(object player, List<ICard> drawnCards)
     {
         foreach (var card in drawnCards)
         {
-            var newCard = Instantiate(cardPrefab, deck.transform.position, deck.transform.rotation);
-            handCards.Add(newCard);
+            CardViewCreator.INSTANCE.CreateAt(
+                card,
+                deck.transform.position,
+                deck.transform.rotation
+            );
             UpdateCardPosition();
         }
     }
 
     private void UpdateCardPosition()
     {
+        var handCards = CardViewRegistry.INSTANCE.GetAll(player.Hand);
+        Debug.Log(handCards.Count + " Cards in Hand of " + player.Name);
         if (handCards.Count == 0)
             return;
-        var spacing = 1f / handCards.Count;
+        var spacing = Math.Min(1f / handCards.Count, 0.05f);
         var firstCardPosition = 0.5f - (handCards.Count - 1) * spacing / 2;
         var spline = splineContainer.Spline;
         for (int i = 0; i < handCards.Count; i++)
         {
             var p = firstCardPosition + i * spacing;
-            var splinePosition = spline.EvaluatePosition(p);
+            var splinePosition = transform.rotation * spline.EvaluatePosition(p);
+            Debug.Log(splinePosition);
             var forward = spline.EvaluateTangent(p);
             var up = spline.EvaluateUpVector(p);
-            var rotation = Quaternion.LookRotation(up, Vector3.Cross(up, forward).normalized);
+            var rotation =
+                transform.rotation
+                * Quaternion.LookRotation(up, Vector3.Cross(up, forward).normalized);
             handCards[i].transform.DOMove(splinePosition, 0.25f);
             handCards[i].transform.DOLocalRotateQuaternion(rotation, 0.25f);
         }
