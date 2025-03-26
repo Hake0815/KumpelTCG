7942b72eca2d3375344e514af47db75c4b149f76
Hake0815 <felix.reichenbach5@web.de>
2025-03-26 18:43:01 +0100

    \#9 add more pokemon card date and build factory

--- Assets/Scripts/card/CardDatabase.cs
+++ Assets/Scripts/card/CardDatabase.cs
@@ -5,14 +5,43 @@ namespace gamecore.card
 {
     public class CardDatabase
     {
-        public static List<ICardData> cardDataList = new()
+        public static Dictionary<string, ICardData> cardDataDict = new()
         {
-            new TrainerCardData(
-                "Bill",
+            {
                 "bill",
-                new List<IEffect> { new DrawCardsEffect(2), new DiscardCardEffect() },
-                new List<IPlayCondition> { new HasCardsInDeck() }
-            ),
+                new TrainerCardData(
+                    "Bill",
+                    "bill",
+                    new List<IEffect> { new DrawCardsEffect(2), new DiscardCardEffect() },
+                    new List<IPlayCondition> { new HasCardsInDeck() }
+                )
+            },
+            {
+                "TWM128",
+                new PokemonCardData(
+                    id: "TWM128",
+                    name: "Dreepy",
+                    stage: Stage.Basic,
+                    type: PokemonType.Dragon,
+                    weakness: PokemonType.None,
+                    resistance: PokemonType.None,
+                    retreatCost: 1,
+                    maxHP: 70,
+                    new List<IAttack>
+                    {
+                        new Attack(
+                            "Petty Grudge",
+                            10,
+                            new List<PokemonType> { PokemonType.Psychic }
+                        ),
+                        new Attack(
+                            " Bite",
+                            40,
+                            new List<PokemonType> { PokemonType.Fire, PokemonType.Psychic }
+                        ),
+                    }
+                )
+            },
         };
     }
 }
