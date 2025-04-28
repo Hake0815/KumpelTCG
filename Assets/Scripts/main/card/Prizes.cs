d0abef139042ff49a94b7502312db8422b09ff5e
Hake0815 <felix.reichenbach5@web.de>
2025-04-28 22:41:18 +0200

    #30 add knock out and prize cards

--- Assets/Scripts/main/card/Prizes.cs
+++ Assets/Scripts/main/card/Prizes.cs
@@ -1,21 +1,38 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace gamecore.card
 {
-    public interface IPrizes : ICardList { }
+    public interface IPrizes : ICardList
+    {
+        event Action<List<ICard>> PrizesTaken;
+    }
 
-    internal interface IPrizesLogic : IPrizes, ICardListLogic { }
+    internal interface IPrizesLogic : IPrizes, ICardListLogic
+    {
+        List<ICardLogic> TakePrizes(int amount);
+    }
 
     internal class Prizes : IPrizesLogic
     {
         public List<ICardLogic> Cards { get; } = new();
 
         public event Action CardCountChanged;
+        public event Action<List<ICard>> PrizesTaken;
 
         public void OnCardCountChanged()
         {
             CardCountChanged?.Invoke();
+        }
+
+        public List<ICardLogic> TakePrizes(int amount)
+        {
+            var prizes = Cards.GetRange(0, Math.Min(amount, Cards.Count));
+            Cards.RemoveRange(0, prizes.Count);
+            OnCardCountChanged();
+            PrizesTaken?.Invoke(prizes.Cast<ICard>().ToList());
+            return prizes;
         }
     }
 }
