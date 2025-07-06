daa38f249e09f92ded1bccb956656c090705c404
Hake0815 <felix.reichenbach5@web.de>
2025-07-06 15:41:23 +0200

    #64 REVERT CARD VIEW COMPARER CHANGES

--- Assets/Scripts/main/views/CardViewComparer.cs
+++ Assets/Scripts/main/views/CardViewComparer.cs
@@ -8,7 +8,6 @@ namespace gameview
     public class CardViewComparer : IComparer<CardView>
     {
         private readonly Predicate<ICard> _cardCondition;
-        private readonly Predicate<CardView> _cardViewCondition;
 
         private CardViewComparer(
             Predicate<ICard> cardCondition,
@@ -16,7 +15,6 @@ namespace gameview
         )
         {
             _cardCondition = cardCondition;
-            _cardViewCondition = cardViewCondition;
         }
 
         public static CardViewComparer Create()
@@ -29,34 +27,8 @@ namespace gameview
             return new CardViewComparer(cardCondition, null);
         }
 
-        public static CardViewComparer Create(Predicate<CardView> cardViewCondition)
-        {
-            return new CardViewComparer(null, cardViewCondition);
-        }
-
         public int Compare(CardView x, CardView y)
         {
-            if (_cardViewCondition is not null)
-            {
-                if (_cardViewCondition(x) && !_cardViewCondition(y))
-                {
-                    Debug.Log(
-                        $"CardViewComparer compared {x.Card.Name} and {y.Card.Name}, returning -1 because {x.Card.Name} matches condition"
-                    );
-                    return -1;
-                }
-                if (!_cardViewCondition(x) && _cardViewCondition(y))
-                {
-                    Debug.Log(
-                        $"CardViewComparer compared {x.Card.Name} and {y.Card.Name}, returning 1 because {y.Card.Name} matches condition"
-                    );
-                    return 1;
-                }
-
-                Debug.Log(
-                    $"CardViewComparer compared {x.Card.Name} and {y.Card.Name}, both don't match condition"
-                );
-            }
             return -x.Card.CompareTo(y.Card, _cardCondition);
         }
     }
