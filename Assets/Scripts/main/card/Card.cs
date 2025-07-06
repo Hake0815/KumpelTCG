f8b9372409f945858f24c6bbb3cd82a01d928370
Hake0815 <felix.reichenbach5@web.de>
2025-07-06 14:06:01 +0200

    #64 rename decksearchview to search view and improve sorting for consistent results

--- Assets/Scripts/main/card/Card.cs
+++ Assets/Scripts/main/card/Card.cs
@@ -24,8 +24,13 @@ namespace gamecore.card
                 return 1;
             if (_cardCondition is not null && !_cardCondition(this) && _cardCondition(other))
                 return -1;
+            return CompareToCardByType(other);
+        }
+
+        int CompareToCardByType(ICard other)
+        {
             if (GetType() == other.GetType())
-                return Name.CompareTo(other.Name);
+                return CompareToEqualType(other);
             if (IsPokemonCard() && !other.IsPokemonCard())
                 return 1;
             if (!IsPokemonCard() && other.IsPokemonCard())
@@ -40,6 +45,15 @@ namespace gamecore.card
                 return -1;
             return 0;
         }
+
+        int CompareToEqualType(ICard other)
+        {
+            var result = Name.CompareTo(other.Name);
+            if (result == 0)
+                return DeckId.CompareTo(other.DeckId);
+            return result;
+        }
+
         int CompareTo(ICard other)
         {
             return CompareTo(other, null);
--- Assets/Scripts/main/views/GameManager.cs
+++ Assets/Scripts/main/views/GameManager.cs
@@ -57,8 +57,8 @@ namespace gameview
         private FloatingSelectionView _floatingSelectionView;
 
         [SerializeField]
-        private DeckSearchView _deckSearchViewPrefab;
-        private DeckSearchView _deckSearchView;
+        private SearchView _searchViewPrefab;
+        private SearchView _searchView;
         private readonly Dictionary<IPlayer, HandView> _playerHandViews = new();
         private readonly Dictionary<IPlayer, DeckView> _playerDeckViews = new();
         private readonly Dictionary<IPlayer, PrizeView> _playerPrizeViews = new();
@@ -77,7 +77,7 @@ namespace gameview
             Instantiate(_cardViewCreatorPrefab);
             Instantiate(_inputHandlerPrefab);
             _floatingSelectionView = Instantiate(_floatingSelectionViewPrefab);
-            _deckSearchView = Instantiate(_deckSearchViewPrefab);
+            _searchView = Instantiate(_searchViewPrefab);
 
             new GameRemoteService(this);
         }
@@ -356,14 +356,14 @@ namespace gameview
             _floatingSelectionView.Clear();
         }
 
-        internal void ActivateDeckSearchView(IDeck deck)
+        internal void ActivateSearchView(List<ICard> cards)
         {
-            _deckSearchView.DisplayCards(deck);
+            _searchView.DisplayCards(cards);
         }
 
-        internal void DisableDeckSearchView()
+        internal void DisableSearchView()
         {
-            _deckSearchView.Clear();
+            _searchView.Clear();
         }
     }
 }
